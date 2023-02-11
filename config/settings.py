88730a85aa3b3ec6afb306fc560f454f74eccfcf
Lutmanova-Ekaterina <elutmanova817@gmail.com>
2023-02-11 14:22:59 +0300

    'test'

--- config/settings.py
+++ config/settings.py
@@ -77,15 +77,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'kursovoy_proekt',
-        'USER': 'postgres',
-        'PASSWORD': '2648',
-        'HOST': 'localhost',
-        'PORT': 5432,
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     },
 }
+
+
 
 
 # Password validation
